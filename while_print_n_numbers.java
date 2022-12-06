44297c77308abf521f2116cf2b10c09bb41c2cc5
Om Bhojane <82753658+ombhojane@users.noreply.github.com>
2022-12-06 06:00:39 -0800

    Create while_print_n_numbers.java
    Print first n natural numbers, where n is the input

---
+++ while_print_n_numbers.java
@@ -0,0 +1,13 @@
+//Print n numbers
+import java.util.Scanner;
+class while_print_n_numbers {
+    public static void main(String[] args) {
+         Scanner sc = new Scanner(System.in);
+         int n = sc.nextInt();
+         int num = 1;
+         while(num <= n){
+         System.out.println(num);
+         num++;
+         }
+    }
+}
